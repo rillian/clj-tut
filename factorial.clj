29eeb8a09e0db3f5d6ca0ee85322227883c327eb
Ralph Giles <giles@mozilla.com>
2014-04-25 01:29:09 -0700

    Repl log when I finally got a factorial to work.

---
+++ factorial.clj
@@ -0,0 +1,9 @@
+;; Anything you type in here will be executed
+;; immediately with the results shown on the
+;; right.
+()
+(inc 4)
+(range 5)
+(range (inc 5))
+(defn factorial [n] (reduce * (range 1 (inc 5))))
+(factorial 5)
