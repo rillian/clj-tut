bb7210624b6f0e7af2bb4025dcbcf8245081a0a3
Ralph Giles <giles@mozilla.com>
2014-04-28 00:07:51 -0700

    Worked through ClojureBridge data types tutorial.

--- clojurebridge.clj
+++ clojurebridge.clj
@@ -4,3 +4,22 @@
 (max 2 17 8)
 
 (def my-hometown "Vancouver")
+
+[1 2 3 4 5]
+["1" 3 4 "b" :keyword]
+[]
+
+; Exercise: Make a vector of places I've lived.
+(def homes ["Vancouver" "London" "Seattle" "Santa Barbara"])
+(nth homes 0)
+(first homes)
+(last homes)
+(rest homes)
+
+(into {} [[:a 2][:b 4]])
+
+; Exercise: Model yourself in a map.
+(def me {:name "Ralph Giles" :nick "rillian"})
+(def more-me (merge me {:hometown (first homes)}))
+(get more-me :hometown "homeless")
+(:hometown more-me)
